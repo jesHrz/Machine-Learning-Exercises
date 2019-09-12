ac226de51535f6a4b15cda0273cd1cfdeef17c8c
jeshrz <huangruizhe@live.cn>
2019-09-12 22:06:02 +0800

    linear regression

--- Exp1/linear_regression.py
+++ Exp1/linear_regression.py
@@ -1,13 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
-def gradient_descent(x, y, theta, learning_rate=0.07, max_iteration=1500):
+# 标准化dt
+def scale(dt):
+    mu = np.mean(dt, axis=0)
+    sigma = np.std(dt, axis=0)
+    return (dt - mu) / sigma
+
+
+# 计算损失函数
+def j_val(X, Y, theta):
+    hx = np.dot(X, theta)
+    return np.dot((hx - Y).transpose(), hx - Y) / (2 * X.shape[0])
+
+
+# 梯度下降
+def gradient_descent(X, Y, theta, learning_rate=0.07, max_iteration=1500):
     # g = ((XO - Y)' * (X))'
+    cost = np.zeros((max_iteration, 1))
     for i in range(max_iteration):
-        hx = np.dot(x, theta)
-        theta = theta - (learning_rate / x.shape[0]) * (np.dot((hx - y).transpose(), x)).transpose()
-    return theta
+        cost[i] = j_val(X, Y, theta)
+        hx = np.dot(X, theta)
+        theta = theta - (learning_rate / X.shape[0]) * (np.dot((hx - Y).transpose(), X)).transpose()
+    return theta, cost
+
+
+def task5():
+    x = np.loadtxt('data/ex1_2x.dat')
+    y = np.loadtxt('data/ex1_2y.dat').reshape(-1, 1)
+    m = x.shape[0]
+    # 将x标准化
+    x = scale(x)
+    x = np.hstack((np.ones((m, 1)), x))
+    m, n = x.shape
+    Theta, cost = gradient_descent(x, y, np.zeros((n, 1)), learning_rate=0.15, max_iteration=50)
+    print(Theta, cost)
+    # plot3
+    plt.figure('Iteration')
+    plt.plot(np.linspace(0, 50, 50), cost)
+    plt.xlabel('number of iterations')
+    plt.ylabel('Cost J')
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -16,16 +51,36 @@ if __name__ == "__main__":
     m = x.shape[0]
     x = np.hstack((np.ones((m, 1)), x))
     m, n = x.shape
-    Theta = gradient_descent(x, y, np.zeros((n, 1)))
+    Theta, cost = gradient_descent(x, y, np.zeros((n, 1)))
     y1 = np.dot(np.array([1, 3.5]), Theta)
     y2 = np.dot(np.array([1, 7]), Theta)
     print("X=3.5\tY=%f" % y1)
     print("X=7.0\tY=%f" % y2)
-    Y = np.dot(x, Theta)
-    # plot
+    learn_y = np.dot(x, Theta)
+    # plot1
     plt.figure('Linear Regression')
     plt.plot(x[:, 1], y, '.')
-    plt.plot(x[:, 1], Y)
+    plt.plot(x[:, 1], learn_y)
     plt.xlabel('Age in years')
     plt.ylabel('Height in meters')
     plt.show()
+
+    # plot2
+    fig2 = plt.figure('J value')
+    theta_0 = np.linspace(-3, 3, 100)
+    theta_1 = np.linspace(-1, 1, 100)
+    j_vals = np.zeros((100, 100))
+    for i in range(100):
+        for j in range(100):
+            j_vals[i][j] = j_val(x, y, np.array([theta_0[i], theta_1[j]]).reshape(-1, 1))
+    theta_0, theta_1 = np.meshgrid(theta_0, theta_1)
+    # surf
+    # ax = Axes3D(fig2)
+    # ax.plot_surface(theta_0, theta_1, j_vals)
+    plt.contour(theta_0, theta_1, j_vals)
+    plt.xlabel('theta_0')
+    plt.ylabel('theta_1')
+    plt.show()
+
+    task5()
+
--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# matplotlib.pyploy 绘图
+# Linear Regression - 0912
+
+1. 从文件`ex1_1x.dat`和`ex1_1y.dat`中读取数据并描绘散点图
+
+2. 为x增添一维后利用全梯度下降求出使损失函数$J(\theta)$最小的参数$\theta$并绘图，结果见`result1.png`。然后对x=3.5和x=7时做预测
+
+3. 绘制$\theta_0 - \theta_1 - J$的函数图像
+
+4. 对第二组数据`ex1_2x.dat`与`ex1_2y.dat`中的x矩阵做标准化处理，调整学习率`learning_rate`使每次迭代计算出的损失函数较优，结果见`result2.png`
+
+### matplotlib.pyploy 绘图
 
 figure() 设置画布
 
-scatter(x, y) 设置数据
+plot(x, y) 设置数据
 
 xlabel() 设置x坐标标签
 
@@ -10,5 +20,28 @@ ylabel() 设置y坐标标签
 
 show() **展示画布**
 
-# numpy 读dat文件
+Axes3D.plot_surface(x, y, z) 绘制三维坐标图
+
+Axes3D.contour(x, y, z) 绘制等高线图
+
+### numpy 读dat文件
+
 loadtxt()
+
+### numpy 矩阵操作
+
+hstack((x, y)) x矩阵与y矩阵按行拼接
+
+vstack((x, y)) 按列拼接
+
+reshape(-1, 1) 转换为列向量
+
+x.shape 返回tuple表示 (列，行)
+
+dot(x, y) 如果xy是数组则做内积，为矩阵则做矩阵乘法
+
+transpose() 求转置矩阵
+
+std(x, axis) 沿第axis个变量的方向求标准差
+
+mean(x, axis) 沿第axis个变量的方向求平均值
