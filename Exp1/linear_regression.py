42f325e5be6848cec511586136971c514421f15b
jeshrz <huangruizhe@live.cn>
2019-09-20 09:06:12 +0800

    update contour

--- Exp1/linear_regression.py
+++ Exp1/linear_regression.py
@@ -20,13 +20,11 @@ def j_val(X, Y, theta):
 def gradient_descent(X, Y, theta, learning_rate=0.07, max_iteration=1500):
     # g = ((XO - Y)' * (X))'
     cost = np.zeros((max_iteration, 1))
-    _theta = np.zeros((max_iteration, theta.shape[0]))
     for i in range(max_iteration):
         cost[i] = j_val(X, Y, theta)
-        _theta[i] = theta.transpose()
         hx = np.dot(X, theta)
         theta = theta - (learning_rate / X.shape[0]) * (np.dot((hx - Y).transpose(), X)).transpose()
-    return theta, cost, _theta
+    return theta, cost
 
 
 def task5():
@@ -40,24 +38,31 @@ def task5():
 
     n = x.shape[1]
     max_iteration = 50
-    Theta, cost, _theta = gradient_descent(x[0: x.shape[0] - 1], y, np.zeros((n, 1)),
-                                           learning_rate=0.15, max_iteration=max_iteration)
-    print("Theta =", Theta.transpose())
+    Theta_1, cost_1 = gradient_descent(x[0: x.shape[0] - 1], y, np.zeros((n, 1)),
+                                       learning_rate=0.05, max_iteration=max_iteration)
+    Theta_2, cost_2 = gradient_descent(x[0: x.shape[0] - 1], y, np.zeros((n, 1)),
+                                       learning_rate=0.15, max_iteration=max_iteration)
+    Theta_3, cost_3 = gradient_descent(x[0: x.shape[0] - 1], y, np.zeros((n, 1)),
+                                       learning_rate=0.45, max_iteration=max_iteration)
+    Theta_4, cost_4 = gradient_descent(x[0: x.shape[0] - 1], y, np.zeros((n, 1)),
+                                       learning_rate=1.2, max_iteration=max_iteration)
+    Theta_5, cost_5 = gradient_descent(x[0: x.shape[0] - 1], y, np.zeros((n, 1)),
+                                       learning_rate=0.005, max_iteration=max_iteration)
+    print("Theta =", Theta_2.transpose())
 
-    print("prediction[1650, 3] =", np.dot(Theta.transpose(), x[x.shape[0] - 1].transpose()))
+    print("prediction[1650, 3] =", np.dot(Theta_2.transpose(), x[x.shape[0] - 1].transpose()))
 
     # 每次迭代损失函数的值
     plt.figure('cost')
-    plt.plot(np.linspace(0, max_iteration, max_iteration), cost)
+    plt.plot(np.linspace(0, max_iteration, max_iteration), cost_1, label='0.05')
+    plt.plot(np.linspace(0, max_iteration, max_iteration), cost_2, label='0.15')
+    plt.plot(np.linspace(0, max_iteration, max_iteration), cost_3, label='0.45')
+    plt.plot(np.linspace(0, max_iteration, max_iteration), cost_4, label='1.20')
+    plt.plot(np.linspace(0, max_iteration, max_iteration), cost_5, label='0.005')
+
     plt.xlabel('number of iterations')
     plt.ylabel('Cost J')
-    plt.show()
-
-    # 每次迭代的theta
-    plt.figure('theta')
-    plt.plot(np.linspace(0, max_iteration, max_iteration), _theta[:, 0])    # theta_0
-    plt.plot(np.linspace(0, max_iteration, max_iteration), _theta[:, 1])    # theta_1
-    plt.plot(np.linspace(0, max_iteration, max_iteration), _theta[:, 2])    # theta_2
+    plt.legend()
     plt.show()
 
 
@@ -68,7 +73,7 @@ if __name__ == "__main__":
     x = np.hstack((np.ones((x.shape[0], 1)), x))
 
     m, n = x.shape
-    Theta, cost, _theta = gradient_descent(x, y, np.zeros((n, 1)))
+    Theta, cost = gradient_descent(x, y, np.zeros((n, 1)))
     print("Theta =", Theta.transpose())
 
     # 对x=3.5和x=7做预测
@@ -79,10 +84,11 @@ if __name__ == "__main__":
 
     # 散点图与拟合直线
     plt.figure('Linear Regression')
-    plt.plot(x[:, 1], y, '.')   # 散点
-    plt.plot(x[:, 1], np.dot(x, Theta))  # 直线
+    plt.plot(x[:, 1], y, '.', label='origin')   # 散点
+    plt.plot(x[:, 1], np.dot(x, Theta), label='regression')  # 直线
     plt.xlabel('Age in years')
     plt.ylabel('Height in meters')
+    plt.legend()
     plt.show()
 
     # 损失函数可视化
@@ -96,9 +102,11 @@ if __name__ == "__main__":
     theta_0, theta_1 = np.meshgrid(theta_0, theta_1)
 
     # 三维图
-    Axes3D(fig2).plot_surface(theta_0, theta_1, j_vals)
+    # Axes3D(fig2).plot_surface(theta_0, theta_1, j_vals)
     # 等高线
-    # plt.contour(theta_0, theta_1, j_vals)
+    plt.contourf(theta_0, theta_1, j_vals, 40, alpha=0.8)
+    cb = plt.colorbar()
+    cb.set_label('J')
     plt.xlabel('theta_0')
     plt.ylabel('theta_1')
     plt.show()
